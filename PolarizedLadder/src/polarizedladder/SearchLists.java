512a90fe25daa452d932e4820dc2d4aaeeabe668
ede <ede.cameron@gmail.com>
2013-04-08 16:26:20 -0400

    Added Method cloneOpenList
    cloneOpenList - takes this list and a point to be removed as args
    copies the list and removes the point from the copied list- returns the
    copy

--- PolarizedLadder/src/polarizedladder/SearchLists.java
+++ PolarizedLadder/src/polarizedladder/SearchLists.java
@@ -1,5 +1,8 @@
 package polarizedladder;
 
+import java.awt.Point;
+import java.util.Hashtable;
+
 public class SearchLists extends GameHeuristics {
 	
 	public SearchLists(){
@@ -7,4 +10,12 @@ public class SearchLists extends GameHeuristics {
 		super();
 	}
 
+	
+	public Hashtable<Point, String> cloneOpenList( Hashtable<Point, String> openList, Point removePoint){
+		Hashtable<Point, String> copy =	openList;
+		if(copy.containsValue(removePoint))
+		  copy.remove(removePoint);
+		return copy;
+	}
+	
 }
